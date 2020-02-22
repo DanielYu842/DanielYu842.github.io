d680e59ed96bfdd5aa6669bd26bd2e9e09a420dc
Daniel Yu <danielyu2004@icloud.com>
2020-02-21 23:21:17 -0500

    d

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css'
+class FooterRight extends React.Component{
+    render(){
+        return(
+            <div class="footer-div">
+                <div class="footer-copyright">©️ Daniel Yu | All Rights Reserved <br/>Developed as a side project</div>
+            </div>
+        )
+    }
+}
+
+export default FooterRight
