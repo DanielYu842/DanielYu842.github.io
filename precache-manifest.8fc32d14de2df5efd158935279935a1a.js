3b9bcc222f7e5cf2c60956ed47756169f1e0f857
Daniel Yu <danielyu2004@icloud.com>
2020-02-21 22:29:29 -0500

    Updates

---
+++ precache-manifest.8fc32d14de2df5efd158935279935a1a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63125f9f8ad4225c68bf6ce91e1e6031",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21ae79d25826509ff229",
+    "url": "/static/css/main.bd198d00.chunk.css"
+  },
+  {
+    "revision": "9220be2afb713b63a1f0",
+    "url": "/static/js/2.50ecbbe9.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.50ecbbe9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21ae79d25826509ff229",
+    "url": "/static/js/main.326b41e3.chunk.js"
+  },
+  {
+    "revision": "f95d4c768a1c35663f22",
+    "url": "/static/js/runtime-main.629412b4.js"
+  },
+  {
+    "revision": "9fae9f68463c60ce28f7dd71ca88ca42",
+    "url": "/static/media/AboutBackgroundgood.9fae9f68.png"
+  },
+  {
+    "revision": "22a4b0f0b03978ca07b59095c133f972",
+    "url": "/static/media/AwardsBackground (2).22a4b0f0.jpg"
+  },
+  {
+    "revision": "3542119dfa23d7aa1a375871666ed4ef",
+    "url": "/static/media/Image2.3542119d.png"
+  },
+  {
+    "revision": "636cfc10bdac3e501a90c98ab48e0ac8",
+    "url": "/static/media/ProjectsBackground.636cfc10.jpg"
+  },
+  {
+    "revision": "7fd7a7af5b87142aa89601ea80c84875",
+    "url": "/static/media/algorithm.7fd7a7af.svg"
+  },
+  {
+    "revision": "1f647cefb84fc42f68f44a96170943ba",
+    "url": "/static/media/background.1f647cef.jpg"
+  },
+  {
+    "revision": "e49c0cb1100bbc61072f03ce1b64d115",
+    "url": "/static/media/browsericon.e49c0cb1.svg"
+  },
+  {
+    "revision": "9a8c0e835976c8a40b6edbadcd16d8c1",
+    "url": "/static/media/server.9a8c0e83.svg"
+  }
+]);
