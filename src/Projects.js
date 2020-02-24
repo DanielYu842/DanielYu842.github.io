b05ad061a26266ef99989795adea021eaf8963e0
Daniel Yu <danielyu2004@icloud.com>
2020-02-23 23:27:44 -0500

    d

--- src/Projects.js
+++ src/Projects.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './Projects.css'
 import Footer from './Footer'
 import ProjectItem from './ProjectItem'
+import Whistler from './Assets/Whistler.jpg'
+
 
 class Projects extends React.Component{
     render(){
@@ -11,9 +13,23 @@ class Projects extends React.Component{
                     <div class="projects-title">My Projects</div>
 
                     <div class="projects-content">
-                        <ProjectItem image="./Assets/Visionary Image.png"></ProjectItem>
-                        <ProjectItem image="./Assets/Visionary Image.png"></ProjectItem>
-                        <ProjectItem image="./Assets/Visionary Image.png"></ProjectItem>
+                        <ProjectItem 
+                            title="Visionary Development Studios" 
+                            image="./Assets/Visionary Image.png" 
+                            link="https://google.com" 
+                            description="Visionary is a SaaS model web application that will be the first social platform where users can plan, pledge, commit, and act on setting the planet to the path of environmental sustainability."
+                            technologies="React | Javascript | Figma  Photoshop | Illustrator"
+                            ></ProjectItem>
+                        <ProjectItem 
+                            image={Whistler}
+                            title="Whistler"
+                            description="A blockchain web application that allows users to securely and anonymously upload whistleblower complaints using the Inter Planetary File System and Metamask authentication"
+                            technologies={<div>React | Javascript | Node.js <br></br>  Fetch API | Illustrator</div>}
+                            ></ProjectItem>
+                        <ProjectItem 
+                            image="./Assets/Visionary Image.png"
+
+                            ></ProjectItem>
 
                     </div>
 
