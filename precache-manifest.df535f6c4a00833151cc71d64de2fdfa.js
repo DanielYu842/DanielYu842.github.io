c4ab0c6b0d853bb747161bac429f4da98f4c1c23
Daniel Yu <danielyu2004@icloud.com>
2020-02-28 09:34:06 -0500

    Revert "Update precache-manifest.df535f6c4a00833151cc71d64de2fdfa.js"
    This reverts commit dba8f851e59f74a09caab0c998794c1206e50c12.

--- precache-manifest.df535f6c4a00833151cc71d64de2fdfa.js
+++ precache-manifest.df535f6c4a00833151cc71d64de2fdfa.js
@@ -1,11 +1,20 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
+<<<<<<< HEAD:precache-manifest.9901648904c070aa3041ac86cf1cab40.js
     "revision": "dffe98a3c3e88d1e64d70c897ee9777e",
     "url": "/index.html"
   },
   {
     "revision": "200eeaf0f29cf30f8962",
     "url": "/static/css/main.9a1eb5a1.chunk.css"
+=======
+    "revision": "0b79113c6fc95b65a16a0b74debc3bf1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54831be39ed02cf5674f",
+    "url": "/static/css/main.f9de88e3.chunk.css"
+>>>>>>> 49076e217b5d642cd14271c792368c8f52d5e61e:precache-manifest.df535f6c4a00833151cc71d64de2fdfa.js
   },
   {
     "revision": "9220be2afb713b63a1f0",
@@ -16,8 +25,13 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.50ecbbe9.chunk.js.LICENSE.txt"
   },
   {
+<<<<<<< HEAD:precache-manifest.9901648904c070aa3041ac86cf1cab40.js
     "revision": "200eeaf0f29cf30f8962",
     "url": "/static/js/main.ea82a635.chunk.js"
+=======
+    "revision": "54831be39ed02cf5674f",
+    "url": "/static/js/main.c902fe46.chunk.js"
+>>>>>>> 49076e217b5d642cd14271c792368c8f52d5e61e:precache-manifest.df535f6c4a00833151cc71d64de2fdfa.js
   },
   {
     "revision": "f95d4c768a1c35663f22",
@@ -40,8 +54,25 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/ProjectsBackground.636cfc10.jpg"
   },
   {
+<<<<<<< HEAD:precache-manifest.9901648904c070aa3041ac86cf1cab40.js
     "revision": "7fd7a7af5b87142aa89601ea80c84875",
     "url": "/static/media/algorithm.7fd7a7af.svg"
+=======
+    "revision": "48b6bc44bc5533e19fa527f2370ba2c8",
+    "url": "/static/media/Resume.48b6bc44.pdf"
+  },
+  {
+    "revision": "6f1d2104aaf589ca6fbcae361324e9b0",
+    "url": "/static/media/VisionaryImage.6f1d2104.jpg"
+  },
+  {
+    "revision": "0567d782a60b0a39778b7dd5d83461d5",
+    "url": "/static/media/Whistler.0567d782.jpg"
+  },
+  {
+    "revision": "e9e63d127058e552662c00abd3a90851",
+    "url": "/static/media/algorithm.e9e63d12.svg"
+>>>>>>> 49076e217b5d642cd14271c792368c8f52d5e61e:precache-manifest.df535f6c4a00833151cc71d64de2fdfa.js
   },
   {
     "revision": "1f647cefb84fc42f68f44a96170943ba",
