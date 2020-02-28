495e75b673c806e1d691cba64b820289efad1a0e
Daniel Yu <danielyu2004@icloud.com>
2020-02-27 23:43:06 -0500

    Update About.js

--- src/About.js
+++ src/About.js
@@ -10,7 +10,7 @@ class About extends React.Component{
                 <div class="about-div-content">
                     <div class="about-div-text">
                         <div class="about-title">
-                            About Me 
+                            About Me test
                         </div>
                         <div class="about-content">
                         I am a grade 10 student in Waterloo Ontario with a strong passion for software development. I enjoy competitive coding problems with python while honing my skills in software development with react as a front-end developer. I have experience at over 7 hackathons (with a few wins) and have mentored at various coding seminars. In my spare time, I like to play chess, develop my competitive math skills and learn more about the world of business.
