b29a22cbbeb936f64900aca0023a267dd9def185
Daniel Yu <danielyu2004@icloud.com>
2020-03-23 14:01:12 -0400

    Updates

---
+++ precache-manifest.d0915ab6bbefdcf7a98ff335d5c90a48.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec2db0355c7677cddbf1807b6b14b95f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "352e212b918bae4711b8",
+    "url": "/static/css/main.0f91d434.chunk.css"
+  },
+  {
+    "revision": "5d9e700fb222e65447ac",
+    "url": "/static/js/2.9c03b874.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.9c03b874.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "352e212b918bae4711b8",
+    "url": "/static/js/main.0c36466e.chunk.js"
+  },
+  {
+    "revision": "d2ecca1f82890566dcd5",
+    "url": "/static/js/runtime-main.430abd34.js"
+  },
+  {
+    "revision": "f2bf9e5fcc71f11e69a4ae9174937284",
+    "url": "/static/media/About Profile.f2bf9e5f.png"
+  },
+  {
+    "revision": "9fae9f68463c60ce28f7dd71ca88ca42",
+    "url": "/static/media/AboutBackgroundgood.9fae9f68.png"
+  },
+  {
+    "revision": "22a4b0f0b03978ca07b59095c133f972",
+    "url": "/static/media/AwardsBackground (2).22a4b0f0.jpg"
+  },
+  {
+    "revision": "80d3669e4c1eb3c465fccf84c7b016d8",
+    "url": "/static/media/Organotes.80d3669e.PNG"
+  },
+  {
+    "revision": "636cfc10bdac3e501a90c98ab48e0ac8",
+    "url": "/static/media/ProjectsBackground.636cfc10.jpg"
+  },
+  {
+    "revision": "48b6bc44bc5533e19fa527f2370ba2c8",
+    "url": "/static/media/Resume.48b6bc44.pdf"
+  },
+  {
+    "revision": "6f1d2104aaf589ca6fbcae361324e9b0",
+    "url": "/static/media/VisionaryImage.6f1d2104.jpg"
+  },
+  {
+    "revision": "0567d782a60b0a39778b7dd5d83461d5",
+    "url": "/static/media/Whistler.0567d782.jpg"
+  },
+  {
+    "revision": "e9e63d127058e552662c00abd3a90851",
+    "url": "/static/media/algorithm.e9e63d12.svg"
+  },
+  {
+    "revision": "1f647cefb84fc42f68f44a96170943ba",
+    "url": "/static/media/background.1f647cef.jpg"
+  },
+  {
+    "revision": "e0d4cba185c0244cbd8a203226456129",
+    "url": "/static/media/browsericon.e0d4cba1.svg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/static/media/github-logo.76d344f0.svg"
+  },
+  {
+    "revision": "bea2ce050a43c8f3a773e57ef1211119",
+    "url": "/static/media/python-file.bea2ce05.svg"
+  },
+  {
+    "revision": "9a8c0e835976c8a40b6edbadcd16d8c1",
+    "url": "/static/media/server.9a8c0e83.svg"
+  }
+]);
