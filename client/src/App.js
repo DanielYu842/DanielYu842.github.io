5731fb50d438ad67afb17651a74318ef31c26482
Daniel Yu <danielyu2004@icloud.com>
2020-04-02 19:07:01 -0400

    d

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <div className="navbar-div">
+          <div className="logo">daniel yu</div>
+        </div>
+        <div className="landing-div">    
+          <div className="title-div">
+            <div className="title-content">            
+              <div className="title-1">Hey, I'm Daniel</div>
+              <div className="title-2">I AM A DEVELOPER</div>
+              <div className="title-3">seeking development related opportunities</div>
+            </div>
+          </div>    
+          <div className="filler-div1"></div>
+        </div>
+        <div className="skills-div">
 
-    </div>
-  );
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
