6d989f9cda11ad13eea19a5581d2d3ae42fc2fd8
Daniel Yu <danielyu2004@icloud.com>
2020-04-02 20:12:28 -0400

    d

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import '../node_modules/aos/dist/aos.css'; 
 import './App.css';
-
+import AOS from 'aos'; 
+import Card from './card'
 class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {}
+    AOS.init(); 
+
+  }
   render(){
     return (
       <div className="App">
@@ -14,16 +22,22 @@ class App extends React.Component {
               <div className="title-1">Hey, I'm Daniel</div>
               <div className="title-2">I AM A DEVELOPER</div>
               <div className="title-3">seeking development related opportunities</div>
+              <div className="button-div">RESUME</div>
             </div>
           </div>    
           <div className="filler-div1"></div>
         </div>
         <div className="skills-div">
+          <div className="card1">          
+            <Card></Card>
+          </div>
 
         </div>
       </div>
     );
   }
 }
+
+//https://codepen.io/abdulrazakshaikh/pen/Kzaabo
 
 export default App;
