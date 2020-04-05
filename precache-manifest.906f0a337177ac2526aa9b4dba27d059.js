a6d178c2f0b2dc3194e72d536d25cd9bedcbb325
DanielYu2004 <DanielYu2004@users.noreply.github.com>
2020-04-05 14:18:40 -0400

    Updates

---
+++ precache-manifest.906f0a337177ac2526aa9b4dba27d059.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "040e73b935c65f92d5849eb9701661ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1892b9497ca15673cdb8",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "ac6fe8940773112b31b7",
+    "url": "/static/css/main.7820b78f.chunk.css"
+  },
+  {
+    "revision": "1892b9497ca15673cdb8",
+    "url": "/static/js/2.b42c3d85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b42c3d85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac6fe8940773112b31b7",
+    "url": "/static/js/main.fb199ed0.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  },
+  {
+    "revision": "c79245ae3412e2d11e760ea28b747ee0",
+    "url": "/static/media/CovaidBackground.c79245ae.svg"
+  },
+  {
+    "revision": "0815f337601f3284cdfbf8497902048d",
+    "url": "/static/media/PythonBackground.0815f337.svg"
+  },
+  {
+    "revision": "bae0af94f5cf81766ce476bf831e9def",
+    "url": "/static/media/RESUME2.bae0af94.pdf"
+  },
+  {
+    "revision": "5683506d2f5b1616aec90ebff578d5cf",
+    "url": "/static/media/SimilarnymBackground.5683506d.svg"
+  },
+  {
+    "revision": "e8d38d9aaa8047590a258b170e49ac22",
+    "url": "/static/media/Visionary-A2.ea196a40.png"
+  },
+  {
+    "revision": "694e2183df2e5e17fadc88e4953f5d7a",
+    "url": "/static/media/VisionaryBackground.694e2183.svg"
+  },
+  {
+    "revision": "63f88205651554d164424baa3b07bd9a",
+    "url": "/static/media/WhistlerBackground.63f88205.svg"
+  },
+  {
+    "revision": "0881b76212a0c45f218792707506d442",
+    "url": "/static/media/background.0881b762.svg"
+  },
+  {
+    "revision": "9f14742cfdaf2f4658a0aaa12d78d010",
+    "url": "/static/media/background1.9f14742c.svg"
+  },
+  {
+    "revision": "6c9b3236fecb98d8923e72421dbd1deb",
+    "url": "/static/media/covaid.9042e929.png"
+  },
+  {
+    "revision": "058a5b6f0e4bed8f73f20c466c1f705d",
+    "url": "/static/media/github.058a5b6f.svg"
+  },
+  {
+    "revision": "89ccb09060a7adf788ea2340c54d6d78",
+    "url": "/static/media/nodejs.89ccb090.svg"
+  },
+  {
+    "revision": "9c0a9fdd024e43413e5b09f8b11d6f5f",
+    "url": "/static/media/ocean.9c0a9fdd.svg"
+  },
+  {
+    "revision": "b9bbb1d722d3ef6ebf7d573703b5925e",
+    "url": "/static/media/python-logo.f0c9d647.png"
+  },
+  {
+    "revision": "c3fbd71b12b726e00eec2b59f3c76786",
+    "url": "/static/media/react-2.c3fbd71b.svg"
+  },
+  {
+    "revision": "0b8228ffe94353daedf7afa41bc12ff4",
+    "url": "/static/media/webdev.0b8228ff.svg"
+  },
+  {
+    "revision": "9dcd72480c1f5f3fda3994605492f2f8",
+    "url": "/static/media/whistlerLogo.9dcd7248.svg"
+  }
+]);
