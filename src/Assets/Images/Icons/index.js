fec2287db81d0855b4eb88021d3505b3bcf183b6
Daniel Yu <55118464+DanielYu2004@users.noreply.github.com>
2021-03-22 23:51:52 -0400

    Updates

--- src/Assets/Images/Icons/index.js
+++
@@ -1,5 +0,0 @@
-import { ReactComponent as Arrow } from "./Arrow.svg";
-import { ReactComponent as Github } from "./Github.svg";
-import { ReactComponent as Linkedin } from "./Linkedin.svg";
-
-export { Arrow, Github, Linkedin };
